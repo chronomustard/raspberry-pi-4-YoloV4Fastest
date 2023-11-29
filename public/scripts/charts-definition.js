9ae458d8905712ab6c40c975e40ed0574719556b
chronomustard <ricky.rahman@ui.ac.id>
2023-11-29 23:13:03 +0700

    Latest commit 29/11/2023

---
+++ public/scripts/charts-definition.js
@@ -0,0 +1,89 @@
+// Create the charts when the web page loads
+window.addEventListener('load', onload);
+
+function onload(event){
+  chartT = createTemperatureChart();
+  chartH = createHumidityChart();
+}
+
+// Create Temperature Chart
+function createTemperatureChart() {
+  var chart = new Highcharts.Chart({
+    chart:{ 
+      renderTo:'chart-temperature',
+      type: 'spline' 
+    },
+    series: [
+      {
+        name: 'Class Capacity'
+      }
+    ],
+    title: { 
+      text: undefined
+    },
+    plotOptions: {
+      line: { 
+        animation: false,
+        dataLabels: { 
+          enabled: true 
+        }
+      },
+      series: { 
+        color: '#0f0f0f' 
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { second: '%H:%M:%S' }
+    },
+    yAxis: {
+      title: { 
+        text: 'Percentage (%)' 
+      }
+    },
+    credits: { 
+      enabled: false 
+    }
+  });
+  return chart;
+}
+
+// Create Humidity Chart
+function createHumidityChart(){
+  var chart = new Highcharts.Chart({
+    chart:{ 
+      renderTo:'chart-humidity',
+      type: 'spline'  
+    },
+    series: [{
+      name: 'Class Attendance'
+    }],
+    title: { 
+      text: undefined
+    },    
+    plotOptions: {
+      line: { 
+        animation: false,
+        dataLabels: { 
+          enabled: true 
+        }
+      },
+      series: { 
+        color: '#0f0f0f' 
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { second: '%H:%M:%S' }
+    },
+    yAxis: {
+      title: { 
+        text: 'People' 
+      }
+    },
+    credits: { 
+      enabled: false 
+    }
+  });
+  return chart;
+}
